f4103fa41cbeab0cfc50bb434a2b896cb7d800dd
Jasmine Fan <lyspfan@gmail.com>
2026-03-13 18:56:01 -0400

    add queries that preprocess pfx2as prefix column to match my net id format

--- psql/pfx2as.sql
+++ psql/pfx2as.sql
@@ -3,3 +3,8 @@ CREATE TABLE pfx2as (
     PrefixLen     smallint,
     ASN           text
 );
+
+ALTER TABLE pfx2as ALTER COLUMN prefix TYPE text USING CAST ( prefix AS text );
+UPDATE pfx2as SET prefix = (regexp_replace(prefix, '/.*', ''));
+UPDATE pfx2as SET prefix = exploded(prefix);
+UPDATE pfx2as SET prefix = left(prefix, 16);
