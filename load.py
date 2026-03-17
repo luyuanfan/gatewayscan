832ba2f0ca1845d6f1d3644d37dea0f3cfe065b7
Jasmine Fan <lyspfan@gmail.com>
2026-03-17 18:37:34 -0400

    update load.py

--- load.py
+++ load.py
@@ -8,13 +8,14 @@ import psycopg2
 import ipaddress
 import pandas as pd
 from math import log2
-import logging as log
 import multiprocessing as mp
 from collections import Counter
 from sqlalchemy import create_engine
 
-data_dir='/dbdata/'
+source_data_dir='/mnt/usb'
 tablename='test2'
+data_dir='/dbdata/'
+nproc = 30
 
 def entropy_hex(hid):
     c = Counter(hid)
@@ -46,7 +47,9 @@ def process_row(row, pfxlen):
     }
 
 def check(chunk, pfxlen):
-    # print(f'')
+    # while chunk.readline().startswith('#'):
+
+
     colnames = ['protocol', 'tgtip', 'srcip', 'hoplim', 'icmpv6type', 'icmpv6code', 'rtt']
     df = pd.read_csv(chunk, names=colnames, header=None)
     results = df.apply(process_row, args=(pfxlen, ), axis=1)
@@ -54,40 +57,48 @@ def check(chunk, pfxlen):
 
 def main():
     if len(sys.argv) < 2:
-        log.error('Usage: python3 load.py <input_file1> <inputfile2> ...')
+        print('Usage: python3 load.py <filename1> <filename2> ...')
+        print('    or python3 load.py --full')
         sys.exit(1)
-    for filepath in sys.argv[1:]:
-        if not os.path.exists(filepath):
-            log.error(f'{filepath} not found, please check the name')
-            sys.exit(1)
-        if not (filepath.endswith('.csv.bz2') or filepath.endswith('.csv')):
-            log.error('Invalid file format. It should only end with .csv or .csv.bz2')
-            sys.exit(1)
-    
+    if ("--full" not in sys.argv[1:]):
+        print('Using test mode')
+        pathlist = sys.argv[1:]
+        load_all = False
+    else:
+        print('Using full mode')
+        pathlist = os.listdir(source_data_dir)
+        load_all = True
+
     # os.system(f"mkdir -p {data_dir}")
     # os.system(f"chmod a+w+r {data_dir}")
-
+    # TODO: should create the database from making the schema
     engine = create_engine('postgresql+psycopg2://lyspfan:lyspfan@localhost:6789/lyspfan')
     conn = engine.raw_connection()
     cur = conn.cursor()
     output = io.StringIO()
 
-    # TODO: think about where we decompress the files? seems like pbzip2 should do it?
-    # and i don't need to deal with work assignment?
-    # TODO: also must remove the first few lines (the comments out)
+    # TODO: also must remove the first few lines (the comments out)!!!!!!!!!!! VERY IMPORYTTANT
+    for filepath in pathlist:
+        if load_all:
+            if not os.path.exists(os.path.join(source_data_dir, filepath)):
+                print(f'{filepath} not found, please check the name')
+                sys.exit(1)
+            if not (filepath.endswith('.csv.bz2') or filepath.endswith('.csv')):
+                print(f'skipping file {filepath}')
+                continue
+            outfilename = filepath.removesuffix('.bz2').removeprefix(f'{source_data_dir}')
+            os.system(f'pbzip2 -dfk -p30 {os.path.join(source_data_dir, filepath)} > {os.path.join(data_dir, outfilename)}')
+            pfxlen = (filepath.removesuffix(".csv.bz2"))[-2:]
+        else:
+            outfilename = os.path.join(data_dir, filepath)
+            pfxlen = 56
 
-    for filepath in sys.argv[1:]:
-        # might want to decide the number of worker depending on 
-        nproc = 30
         pool = mp.Pool(nproc)
 
         start = time.time()
         print(f'Started processing {filepath}')
-
         os.system(f'split {filepath} --number=l/{nproc} --additional-suffix=.csv {data_dir}')
-
-        # TODO: change pfxlen to the actual length
-        pfxlen = 56
+        
         wrs = []
         for chunk in os.listdir(data_dir):
             wr = pool.apply_async(check, (os.path.join(data_dir, chunk), pfxlen, ))
