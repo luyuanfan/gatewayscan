bb75e0de5ac05634ef9d6baeefa0df9beeb8b988
Jasmine Fan <lyspfan@gmail.com>
2026-03-29 21:32:39 -0400

    idk tiny interactive script

---
+++ playground/process.py
@@ -0,0 +1,15 @@
+def main():
+
+    while (1):
+        print("Choose an operation:")
+        print("Help: type 'quit' or 'exit' to leave")
+        print("\t(1) ")
+        print("\t(2) ")
+        print("\t(3) map prefix to asn and organization")
+        answer = input("Option: ")
+        if answer == "quit" or  answer == "exit":
+            print("Exiting program")
+            break
+
+if __name__ == "__main__":
+    main()
