98011e68271a78d5a570db0da303882a9561100d
Jasmine Fan <lyspfan@gmail.com>
2026-03-29 20:58:47 -0400

    update on how we make the indexes (on hostid and a pair of is_slaac and entropy)

--- sql/create_index.sql
+++ sql/create_index.sql
@@ -1,7 +1,6 @@
 \echo 'adding index on all rows that is not slaac and has high entropy score'
-CREATE INDEX IF NOT EXISTS filtered ON :tbl(is_slaac, entropy)
+CREATE INDEX IF NOT EXISTS hostid_idx ON :tbl(is_slaac, entropy)
     WHERE entropy > 0.5 AND is_slaac = False;
 
-\echo 'adding index on source ips'
-CREATE INDEX IF NOT EXISTS srcipidx ON :tbl (srcip)
-    WHERE entropy > 0.5 AND is_slaac = False;
+\echo 'adding index on all host ids'
+CREATE INDEX IF NOT EXISTS hostid_idx ON :tbl (hostid);
