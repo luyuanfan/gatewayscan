565bf9f9af3a0e86e4c33d455c564a19b7b066e3
Jasmine Fan <lyspfan@gmail.com>
2026-03-29 21:35:12 -0400

    add some more processing query to map things together

--- sql/process.sql
+++ sql/process.sql
@@ -17,6 +17,7 @@ FROM full_table
 WHERE hostid IN (
     SELECT hostid
     FROM full_table
+    WHERE entropy > 0.5 AND is_slaac = False
     GROUP BY hostid
     HAVING COUNT(DISTINCT netid) > 1
 );
@@ -34,3 +35,20 @@ SELECT
 FROM duplicate_hostids d
 JOIN pfx2as p ON p.prefix >>= d.subnetpfx;
 
+\echo 'mapping these asn to organization'
+CREATE MATERIALIZED VIEW IF NOT EXISTS hostid_to_org AS
+SELECT
+    h.hostid,
+    h.subnetpfx,
+    h.netid,
+    h.entropy,
+    h.occurrence_count,
+    h.distinct_netid_count,
+    h.prefix,
+    h.asn,
+    a.autname,
+    a.orgid,
+    a.orgname,
+    a.country
+FROM hostid_to_asn h
+JOIN as2org a ON a.aut = h.asn;
