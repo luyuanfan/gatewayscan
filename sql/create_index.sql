f9491feda73b20bfccd7052ac3410c3da79b4314
Jasmine Fan <lyspfan@gmail.com>
2026-03-30 16:06:22 -0400

    index name

--- sql/create_index.sql
+++ sql/create_index.sql
@@ -1,5 +1,5 @@
 \echo 'adding index on all rows that is not slaac and has high entropy score'
-CREATE INDEX IF NOT EXISTS hostid_idx ON :tbl(is_slaac, entropy)
+CREATE INDEX IF NOT EXISTS filtered ON :tbl(is_slaac, entropy)
     WHERE entropy > 0.5 AND is_slaac = False;
 
 \echo 'adding index on all host ids'
