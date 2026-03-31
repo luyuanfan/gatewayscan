e5a3ff711912e5001805ac398d54d8d88b85a5e2
Jasmine Fan <lyspfan@gmail.com>
2026-03-31 16:19:12 -0400

    added queries for both small and full dataset

--- playground/analysis_small.sql
+++ playground/analysis_small.sql
@@ -1,5 +1,5 @@
 # create a table on just the host ids that are duplicated
-CREATE MATERIALIZED VIEW IF NOT EXISTS duplicate_hostids AS
+CREATE MATERIALIZED VIEW IF NOT EXISTS duplicate_hostids_small AS
 SELECT
     hostid,
     tgtip,
@@ -22,10 +22,10 @@ WHERE hostid IN (
     HAVING COUNT(DISTINCT netid) > 1
 );
 
-select hostid, entropy, occurrence_count, subnetpfx, netid, srcip, hoplim, rtt, ICMPv6Type, ICMPv6Code from duplicate_hostids order by entropy desc, hostid desc;
+select hostid, entropy, occurrence_count, subnetpfx, netid, srcip, hoplim, rtt, ICMPv6Type, ICMPv6Code from duplicate_hostids_small order by entropy desc, hostid desc;
 
 # mapping each duplicated hostid prefix to asn
-CREATE MATERIALIZED VIEW IF NOT EXISTS hostid_to_asn AS
+CREATE MATERIALIZED VIEW IF NOT EXISTS hostid_to_asn_small AS
 SELECT
     d.hostid,
     d.subnetpfx,
@@ -34,13 +34,13 @@ SELECT
     d.occurrence_count,
     p.prefix,
     p.asn
-FROM duplicate_hostids d
+FROM duplicate_hostids_small d
 JOIN pfx2as p ON p.prefix >>= d.subnetpfx;
 
-select hostid, entropy, occurrence_count, subnetpfx, netid, asn from hostid_to_asn order by entropy desc, hostid desc;
+select hostid, entropy, occurrence_count, subnetpfx, netid, asn from hostid_to_asn_small order by entropy desc, hostid desc;
 
 # mapping these asn to organization
-CREATE MATERIALIZED VIEW IF NOT EXISTS hostid_to_org AS
+CREATE MATERIALIZED VIEW IF NOT EXISTS hostid_to_org_small AS
 SELECT
     h.hostid,
     h.subnetpfx,
@@ -53,7 +53,7 @@ SELECT
     a.orgid,
     a.orgname,
     a.country
-FROM hostid_to_asn h
+FROM hostid_to_asn_small h
 JOIN as2org a ON a.aut = h.asn;
 
-select hostid, entropy, occurrence_count, subnetpfx, netid, asn, orgid, orgname, country from hostid_to_org order by entropy desc, hostid desc;
+select hostid, entropy, occurrence_count, subnetpfx, netid, asn, orgid, orgname, country from hostid_to_org_small order by entropy desc, hostid desc;
