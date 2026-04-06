889a538c3676d7286e8918a3d3326aad61ff35ba
Jasmine Fan <lyspfan@gmail.com>
2026-04-05 22:17:06 -0400

    add table pfx2as2org

--- sql/analysis_queries.sql
+++ sql/analysis_queries.sql
@@ -24,36 +24,62 @@ WHERE hostid IN (
 
 select hostid, entropy, occurrence_count, subnetpfx, netid, srcip, hoplim, rtt, ICMPv6Type, ICMPv6Code from duplicate_hostids order by entropy desc, hostid desc;
 
+create index if not exists duplicated_hostids_idx on duplicate_hostids (hostid);
+
+create index if not exists duplicated_hostid_pfx_idx on duplicate_hostids (subnetpfx);
+
+create table if not exists pfx2as2org
+as (
+    select
+        pfx2as.prefix,
+        pfx2as.PrefixLen,
+        pfx2as.asn,
+        as2org.autname,
+        as2org.orgname,
+        as2org.orgid,
+        as2org.country
+    from pfx2as
+    left join as2org
+    on pfx2as.asn = as2org.aut
+);
+
 # mapping each duplicated hostid prefix to asn
-CREATE MATERIALIZED VIEW IF NOT EXISTS hostid_to_asn AS
+CREATE MATERIALIZED VIEW IF NOT EXISTS duplicated_hostids_to_asn AS
+SELECT
+    d.hostid,
+    d.entropy,
+    d.occurrence_count,
+    d.subnetpfx,
+    p.prefix AS caida_pfx,
+    p.asn AS as_number,
+    d.netid,
+    d.netid_count,
+    d.tgtip,
+    d.srcip,
+    d.hoplim,
+    d.icmpv6type,
+    d.icmpv6code,
+    d.rtt
+FROM duplicate_hostids d
+JOIN pfx2as2org p ON p.prefix >>= d.subnetpfx;
+
+-- select hostid, entropy, occurrence_count, subnetpfx, netid, asn from hostid_to_asn order by entropy desc, hostid desc;
+
+# mapping these asn to organization
+CREATE MATERIALIZED VIEW IF NOT EXISTS hostid_to_org AS
 SELECT
     d.hostid,
     d.subnetpfx,
     d.netid,
     d.entropy,
     d.occurrence_count,
-    p.prefix,
-    p.asn
-FROM duplicate_hostids d
-JOIN pfx2as p ON p.prefix >>= d.subnetpfx;
-
-select hostid, entropy, occurrence_count, subnetpfx, netid, asn from hostid_to_asn order by entropy desc, hostid desc;
-
-# mapping these asn to organization
-CREATE MATERIALIZED VIEW IF NOT EXISTS hostid_to_org AS
-SELECT
-    h.hostid,
-    h.subnetpfx,
-    h.netid,
-    h.entropy,
-    h.occurrence_count,
-    h.prefix,
-    h.asn,
+    d.prefix,
+    d.asn,
     a.autname,
     a.orgid,
     a.orgname,
     a.country
-FROM hostid_to_asn h
-JOIN as2org a ON a.aut = h.asn;
+FROM duplicated_hostids_to_asn d
+JOIN as2org a ON a.aut = d.asn;
 
 select hostid, entropy, occurrence_count, subnetpfx, netid, asn, orgid, orgname, country from hostid_to_org order by entropy desc, hostid desc;
