256abec93a5194a6f5f47fc88c0d981c0540dd02
Jasmine Fan <lyspfan@gmail.com>
2026-04-27 22:08:04 -0400

    added some scripts that maps those funny ff0fe uuids

---
+++ sql/side_quests.sql
@@ -0,0 +1,26 @@
+# see where is 'ff0fe' host ids come from
+create materialized view if not exists funny_ones as
+select *
+from dup_hostids
+where substring(hostid from 7 for 5) = 'ff0fe'
+
+create materialized view if not exists funny_ones_mapped as
+select
+    f.hostid,
+    f.entropy,
+    f.netid,
+    f.occurrence_count,
+    f.netid_count as distinct_net_occurence,
+    f.subnetpfx,
+    p.prefix as caida_pfx,
+    p.asn as as_number,
+    p.autname as aut_name,
+    p.orgname as oranization_name,
+    p.orgid,
+    p.country,
+    f.hoplim,
+    f.icmpv6type,
+    f.icmpv6code,
+    f.rtt
+from funny_ones f
+left join pfx2as2org p on p.prefix >>= f.netid;
