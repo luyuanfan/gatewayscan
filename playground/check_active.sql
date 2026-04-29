5b036979fadd77bdb65a554c03bda16ff9a1e78c
Jasmine Fan <lyspfan@gmail.com>
2026-04-29 14:26:01 -0400

    moved folders around

--- playground/check_active.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-  pid,
-  now() - pg_stat_activity.query_start AS duration,
-  query,
-  state
-FROM pg_stat_activity
-WHERE (now() - pg_stat_activity.query_start) > interval '5 minutes';
-
-SELECT pg_cancel_backend(__pid__);
